c857b5fc6ba255268e2a2979f88fea3bb2e3822b
seva32 <seva32@yahoo.es>
2020-05-05 20:29:06 +0100

    init commit

---
+++ src/js/Message.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Message = () => {
+  return (
+    <div className="content">
+      <p className="description">React, Express, and Webpack Boilerplate App</p>
+      <div className="image-sample"></div>
+    </div>
+  );
+};
+
+export default Message;
